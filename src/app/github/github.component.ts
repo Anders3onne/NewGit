121057850db63ddc6de695d11d0657bd1ae487c7
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-02-07 09:32:00 +0200

    github component insertions

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { User } from "../user";
 import { HttpClient } from "@angular/common/http";
 import { HttpRequestService } from "../repos-request/http-request.service";
-
+import { Repository } from "../repository";
 @Component({
   selector: "app-github",
   templateUrl: "./github.component.html",
@@ -11,7 +11,10 @@ import { HttpRequestService } from "../repos-request/http-request.service";
 })
 export class GithubComponent implements OnInit {
   Users: User;
+  repos: Repository;
   addNew(user) {
+    this.reposRequest.reposRequest(user);
+    this.repos = this.reposRequest.repos;
     this.Users = new User(user.name);
   }
   constructor(
@@ -19,6 +22,7 @@ export class GithubComponent implements OnInit {
     private reposRequest: HttpRequestService
   ) {
     this.Users = new User("");
+    this.repos = new Repository(0, 0, 0, new Date(), "", "");
   }
 
   ngOnInit() {}
